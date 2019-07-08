c01d44247f8a8c8ccc158c4d11bb321b8ac346a9
morewindows0 <morewindows@qq.com>
2019-07-08 21:42:41 +0800

    feat(master):增加type模块(对应type包)的注释

--- src/main/java/org/apache/ibatis/type/TypeHandler.java
+++ src/main/java/org/apache/ibatis/type/TypeHandler.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.type;
 
@@ -25,15 +25,46 @@ import java.sql.SQLException;
  */
 public interface TypeHandler<T> {
 
+  /**
+   * 设置PreparedStatement的指定参数
+   * Java Type => JDBC Type
+   *
+   * @param ps        PreparedStatement对象
+   * @param i         参数占位符的位置
+   * @param parameter 参数
+   * @param jdbcType  JDBC类型
+   * @throws SQLException
+   */
   void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException;
 
   /**
+   * 获得ResultSet的指定字段的值
+   * JDBC Type => Java Type
+   *
    * @param columnName Colunm name, when configuration <code>useColumnLabel</code> is <code>false</code>
    */
   T getResult(ResultSet rs, String columnName) throws SQLException;
 
+  /**
+   * 获得ResultSet指定字段的值
+   * JDBC => Java Type
+   *
+   * @param rs          ResultSet对象
+   * @param columnIndex 字段位置
+   * @return 值
+   * @throws SQLException
+   */
   T getResult(ResultSet rs, int columnIndex) throws SQLException;
 
+  /**
+   * 获得CallableStatement的指定字段的值
+   * JDBC Type => Java Type
+   *
+   * @param cs CallableStatement 对象
+   * @param columnIndex 字段位置
+   * @return 值
+   * @throws SQLException
+   */
   T getResult(CallableStatement cs, int columnIndex) throws SQLException;
 
 }
