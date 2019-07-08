08905c09a0d318caa0537b590223cc68a8f1c7e8
morewindows0 <morewindows@qq.com>
2019-07-08 15:05:01 +0800

    feat(master):增加注解模块(对应annotations包)注释

--- src/main/java/org/apache/ibatis/annotations/Param.java
+++ src/main/java/org/apache/ibatis/annotations/Param.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.annotations;
 
@@ -22,6 +22,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 当映射器方法需多个参数，这个注解可以被应用于映射器方法参数来给每个参数一个名字。
+ * 否则，多参数将会以它们的顺序位置来被命名。比如 #{1}，#{2} 等，这是默认的。
+ * 使用 @Param("person") ，SQL 中参数应该被命名为 #{person}<br/>
  * @author Clinton Begin
  */
 @Documented
